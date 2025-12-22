c31e46015db69533b3da7e79608d4bfa0830b885
Kirdow <10155603+Kirdow@users.noreply.github.com>
2025-12-22 16:57:12 +0100

    chore: move binary file to ./bin

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
+if not exist bin mkdir bin
+
 go generate ./...
 go mod tidy
 go vet ./...
-go build -o wet.exe cmd/cli/main.go
-go build -o test.exe cmd/test/main.go
+go build -o bin\wet.exe cmd/cli/main.go
+go build -o bin\test.exe cmd/test/main.go
 
 rmdir internal\stdlib\std
 
--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+if [[ ! -d "./bin" ]]; then
+    mkdir ./bin
+fi
+
 go generate ./...
 go mod tidy
 go vet ./...
-go build -o wet cmd/cli/main.go
-go build -o test cmd/test/main.go
+go build -o bin/wet cmd/cli/main.go
+go build -o bin/test cmd/test/main.go
 
 rm -rf internal/stdlib/std
